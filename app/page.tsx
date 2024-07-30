98a7cc3428979e1e2e01035d116af10abf8b21cb
wahyulamani03 <wahyulamani03@gmail.com>
2024-07-30 23:59:44 +0000

    handle clik card save to local storage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
+'use client'
+import React, { useState, useEffect } from 'react'
 import Card from "@/components/card";
 import { questions } from "@/lib/data";
 
 export default function Home() {
+  const [clickedCards, setClickedCards] = useState<number[]>([]);
+
+  useEffect(() => {
+    const savedClickedCards = JSON.parse(localStorage.getItem('clickedCards') || '[]');
+    setClickedCards(savedClickedCards);
+  }, []);
+
+  const handleCardClick = (id: number) => {
+    setClickedCards(prev => {
+      const newClickedCards = [...prev, id];
+      localStorage.setItem('clickedCards', JSON.stringify(newClickedCards));
+      return newClickedCards;
+    });
+  };
   return (
     <section className="flex flex-col items-center justify-center">
       <h1 className="text-3xl font-bold mb-6">
@@ -9,7 +25,8 @@ export default function Home() {
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {questions.map((question, index) => (
-          <Card key={index} question={question} />
+          <Card key={index} question={question}  onClick={() => handleCardClick(question.id)}
+          isClicked={clickedCards.includes(question.id)} />
         ))}
       </div>
     </section>
--- components/card.tsx
+++ components/card.tsx
@@ -1,12 +1,18 @@
 import Link from "next/link";
-const Card = ({ question }:{question:Question}) => {
+const Card = ({ question,onClick, isClicked }:{question:Question,onClick:()=>void,isClicked:boolean}) => {
   return (
     <>
-    <Link href={`/game/${question.id}`} className="flex flex-col items-center justify-center">
-      <div className="bg-blue-500 text-white shadow-md rounded-lg px-[90px] py-6 w-full cursor-pointer hover:bg-blue-600 transition-colors duration-300 flex items-center justify-center">
-        <span className="font-bold text-xl mb-2 text-center">{question.id}</span>
-      </div>
-    </Link>
+    {isClicked ? (
+        <div className="bg-gray-500 text-white shadow-md rounded-lg px-[90px] py-6 w-full cursor-not-allowed flex items-center justify-center">
+          <span className="font-bold text-3xl mb-2 text-center">{question.id}</span>
+        </div>
+      ) : (
+        <Link href={`/game/${question.id}`} onClick={onClick} className="flex flex-col items-center justify-center">
+          <div className="bg-blue-500 text-white shadow-md rounded-lg px-[90px] py-6 w-full cursor-pointer hover:bg-blue-600 transition-colors duration-300 flex items-center justify-center">
+            <span className="font-bold text-3xl mb-2 text-center">{question.id}</span>
+          </div>
+        </Link>
+      )}
     </>
   );
 };
