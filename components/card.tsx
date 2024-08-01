2cb5985b664684cfeaac7ca10c3d1112f97dc434
wahyulamani03 <wahyulamani03@gmail.com>
2024-08-01 02:04:06 +0000

    fix bug text large

--- components/card.tsx
+++ components/card.tsx
@@ -34,10 +34,10 @@ const Card = ({ question,onClick, isClicked }:{question:Question,onClick:()=>voi
     <>
     {isClicked ? (
         <div className="bg-gray-500 text-white shadow-md rounded-lg px-[90px] py-6 w-full cursor-not-allowed flex items-center justify-center">
-          <span className="font-bold text-4xl mb-2 text-center">{question.category}</span>
+          <span className="font-bold text-3xl mb-2 text-center">{question.category}</span>
         </div>
       ) : (
-        <Link href={`game-3/game/${question.id}`} onClick={onClick} className="flex flex-col items-center justify-center">
+        <Link href={`game-3/game/${question.id}`} aria-disabled="true" onClick={onClick} className="flex flex-col items-center justify-center">
           <div className="bg-blue-500 text-white shadow-md rounded-lg px-[90px] py-6 w-full cursor-pointer hover:bg-blue-600 transition-colors duration-300 flex items-center justify-center">
             <span className="font-bold text-3xl mb-2 text-center">{question.category}</span>
           </div>
