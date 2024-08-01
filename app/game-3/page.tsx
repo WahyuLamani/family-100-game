720991f6079c88d162bfe6beb574ca5ffde5a07a
wahyulamani03 <wahyulamani03@gmail.com>
2024-08-01 06:46:49 +0000

    edit view game 3

--- app/game-3/page.tsx
+++ app/game-3/page.tsx
@@ -4,7 +4,7 @@ import { questions } from "@/lib/data";
 export default function Family100Home() {
   return (
     <div className="p-12 flex flex-col items-center justify-center">
-      <h1 className="text-3xl font-bold mb-6">Super Family 100 Pramita</h1>
+      <h1 className="text-6xl font-bold mb-[220px] mt-6">Family 100 Pramita</h1>
       <Cards questions={questions} />
     </div>
   );
